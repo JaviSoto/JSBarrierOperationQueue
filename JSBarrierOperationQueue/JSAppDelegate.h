81fe3dffe77fd68e17c557f1638204d2969c2df2
Javier Soto <javier.api@gmail.com>
2012-07-11 20:53:09 -0700

    Initial Commit

---
+++ JSBarrierOperationQueue/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  JSBarrierOperationQueue
+//
+//  Created by Javier Soto on 7/11/12.
+//  Copyright (c) 2012 MindSnacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSBarrierOperationQueueTests/JSBarrierOperationQueueTests.m
@@ -0,0 +1,32 @@
+//
+//  JSBarrierOperationQueueTests.m
+//  JSBarrierOperationQueueTests
+//
+//  Created by Javier Soto on 7/11/12.
+//  Copyright (c) 2012 MindSnacks. All rights reserved.
+//
+
+#import "JSBarrierOperationQueueTests.h"
+
+@implementation JSBarrierOperationQueueTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JSBarrierOperationQueueTests");
+}
+
+@end
